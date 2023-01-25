23fa5cc1a567d34f1231bed580d73713f65db9f9
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-01-25 11:05:58 +0000

    rust code

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use std::env;
+
+use ethers::abi::ParamType;
+
+enum Query {
+    Proof,
+    Active,
+}
+
+fn main() {
+    let mut args = env::args().skip(1);
+    let maybe_query = args.next().as_ref().and_then(|s| match s.as_ref() {
+        "active" => Some(Query::Active),
+        "proof" => Some(Query::Proof),
+        _ => None,
+    });
+    let maybe_abi_data = args.next().and_then(|abi| {
+        let prefix = abi.get(..2)?;
+        if prefix != "0x" {
+            println!("no");
+            return None;
+        }
+        let abi = abi.get(2..)?;
+        hex::decode(abi).ok()
+    });
+
+    let Some(query) = maybe_query else {
+        eprintln!("First arg must be either `proof` or `active`");
+        return;
+    };
+    let Some(abi_data) = maybe_abi_data else {
+        eprintln!("Invalid or no ABI data arg provided, after the mode of operation");
+        return;
+    };
+
+    match query {
+        Query::Active => {
+            let params = [ParamType::Tuple(vec![
+                ParamType::Array(Box::new(ParamType::Address)),
+                ParamType::Array(Box::new(ParamType::Uint(256))),
+                ParamType::Uint(256),
+            ])];
+            let Ok(tokens) = ethers::abi::decode(&params, &abi_data) else {
+                eprintln!("Invalid active valset ABI encoded data");
+                return;
+            };
+            println!("{tokens:#?}");
+        }
+        Query::Proof => {
+            let params = [ParamType::Tuple(vec![
+                ParamType::FixedBytes(32),
+                ParamType::FixedBytes(32),
+                ParamType::Array(Box::new(ParamType::Tuple(vec![
+                    ParamType::FixedBytes(32),
+                    ParamType::FixedBytes(32),
+                    ParamType::Uint(8),
+                ]))),
+            ])];
+            let Ok(tokens) = ethers::abi::decode(&params, &abi_data) else {
+                eprintln!("Invalid valset proof ABI encoded data");
+                return;
+            };
+            println!("{tokens:#?}");
+        }
+    }
+}
